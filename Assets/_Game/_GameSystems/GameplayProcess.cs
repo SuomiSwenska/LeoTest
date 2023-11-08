67fc1bc6c4d8ab28540d99d120c4c8550f4f25c0
Alexey Nikiforov <SuomiSwenska@gmail.com>
2023-11-08 21:17:04 +0700

    Fields regions added

--- Assets/_Game/_GameSystems/GameplayProcess.cs
+++ Assets/_Game/_GameSystems/GameplayProcess.cs
@@ -9,24 +9,31 @@ public class GameplayProcess : MonoBehaviour
 
     public Action OnDeliveryBoxCollision;
 
+    #region Datas
     [SerializeField] private CharacterInitData _characterInitData;
     [SerializeField] private CakeData _cakeData;
+    [Space(10)]
     [SerializeField] private bl_Joystick _bl_Joystick;
     [SerializeField] private CameraFollower _cameraFollower;
     [SerializeField] private Table _table;
     [SerializeField] private List<GameObject> _uiItems;
+    #endregion
 
+    #region Systems
     private EcsWorld _ecsWorld;
     private CharacterControlSystem _characterControlSystem;
     private CakeControlSystem _cakeControlSystem;
     private StackSystem _stackSystem;
+    #endregion
 
+    #region Getters
     public CharacterInitData CharacterInitData { get => _characterInitData; }
     public CakeData CakeData { get => _cakeData; }
     public bl_Joystick Bl_Joystick { get => _bl_Joystick; }
     public CameraFollower CameraFollower { get => _cameraFollower; }
     public Table Table { get => _table; }
     public List<GameObject> UiItems { get => _uiItems; }
+    #endregion
 
     private void Awake()
     {
